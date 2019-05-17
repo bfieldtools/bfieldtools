ee1c24fb363afdc9420272d0886302c30ac89ec4
Rasmus Zetter <rasmus.zetter@aalto.fi>
2019-05-17 14:19:40 +0300

    Fancy visualization for modes

--- laplacian_mesh.py
+++ laplacian_mesh.py
@@ -82,24 +82,43 @@ if __name__ == '__main__':
     from matplotlib.tri import Triangulation
     from scipy.linalg import eigh
     from mayavi import mlab
-
-#    xx = np.linspace(0, 1, 50)
-#    X, Y = np.meshgrid(xx, xx, indexing='ij')
-#    x = X.ravel()
-#    y = Y.ravel()
-#    z = np.zeros_like(x)
-#    print('Triangulating mesh')
-#    tt = Triangulation(x, y)
-
     import trimesh
     import utils
+
+
     mesh = trimesh.load('./example_meshes/10x10_plane_hires.obj')
+
+    mesh.vertices, mesh.faces = trimesh.remesh.subdivide(mesh.vertices, mesh.faces)
+
     boundary_verts, inner_verts, boundary_tris, inner_tris = utils.find_mesh_boundaries(mesh.vertices, mesh.faces, mesh.edges)
+
     L = laplacian_matrix(mesh.vertices, mesh.faces)
     M = mass_matrix(mesh.vertices, mesh.faces)
+
     u, v = eigh(-L.todense()[inner_verts][:,inner_verts], M.todense()[inner_verts][:,inner_verts])
 
     plt.plot(u)
     scalars = np.zeros(L.shape[0])
-    scalars[inner_verts] = v[:, 200]
+    scalars[inner_verts] = v[:, 1]
     mlab.triangular_mesh(*mesh.vertices.T, mesh.faces, scalars=scalars)
+
+    Nmodes = 100
+    limit = np.max(abs(v[:,0]))
+
+    verts= mesh.vertices
+    tris = mesh.faces
+
+    for ii in range(Nmodes):
+        n = int(np.sqrt(Nmodes))
+        i = ii % n
+        j = int(ii/n)
+        print(i,j)
+        x = verts[:,0] + i*12
+        y = verts[:,1]
+        z = verts[:,2] + j*12
+        scalars[inner_verts] = v[:,ii]
+#        scalars[inner] = v[:,4] +v[:,5]
+        s=mlab.triangular_mesh(x,y,z, tris, scalars=scalars) #M[:,70])
+        s.module_manager.scalar_lut_manager.number_of_colors = 256
+        s.module_manager.scalar_lut_manager.data_range = np.array([-limit,limit])
+        s.actor.mapper.interpolate_scalars_before_mapping = True
