b6ceb977c00ca8c33f59cc175cc48159b06e54ea
Joonas Iivanainen <joonas.iivanainen@aalto.fi>
2019-05-28 13:49:50 +0300

    The code is now commented

--- thermal_noise.py
+++ thermal_noise.py
@@ -16,6 +16,7 @@ def compute_current_modes(mesh):
     The modes are normalized so that the squared norm of the stream function gradient
     integrates to 1 over the surface. With this normalization, the resistances
     of the current modes are R_k = 1/(sigma*d), sigma = conductivity, d = thickness.
+    See Zevenhoven et al. (2014).
     
     Parameters:
         
@@ -34,6 +35,7 @@ def compute_current_modes(mesh):
     
     u, v = eigh(-L.todense()[inner_verts][:,inner_verts], M.todense()[inner_verts][:,inner_verts])
     
+    #Normalize the laplacien eigenvectors
     vl = np.zeros(M.shape)
     for i in range(v.shape[1]):
         vl[inner_verts,i] = v[:, i]/np.sqrt(u[i])
@@ -41,7 +43,26 @@ def compute_current_modes(mesh):
     return vl
 
 def compute_dc_Bnoise(mesh, vl, fp, sigma, d, T):
-    kB = 1.38064852e-23
+    '''
+    Computes the magnetic noise at DC due to thermal motion of charge carriers (Jonhson-Nyquist noise)
+    in a relatively thin conductor.
+    
+    Parameters:
+        
+        mesh: mesh object - the surface mesh
+        vl: Nvertices x Nvertices array - the normalized eddy-current modes vl[:,i]
+        fp: Nfieldpoints x 3 array - coordinates of the fieldpoints
+        sigma: single - conductivity of the surface
+        d: single - thickness of the surface
+        T: single - temperature of the surface
+        
+    Returns:
+        
+        B: Nfieldpoints x 3components array - magnetic RMS noise at DC
+    
+    '''
+    
+    kB = 1.38064852e-23 #Boltzmann constant
     
     C = compute_C(mesh, fp)
     
@@ -51,25 +72,50 @@ def compute_dc_Bnoise(mesh, vl, fp, sigma, d, T):
         B[:,0] += (C[:,:,0]@vec)**2
         B[:,1] += (C[:,:,1]@vec)**2
         B[:,2] += (C[:,:,2]@vec)**2
-    B = np.sqrt(B)
+        
+    B = np.sqrt(B) #RMS
     
     return B
 
 def compute_ac_Bnoise(mesh, vl, fp, freqs, sigma, d, T):
-    kB = 1.38064852e-23
+    '''
+    Computes the AC magnetic noise due to thermal motion of charge carriers (Jonhson-Nyquist noise)
+    in a relatively thin conductor.
     
+    Parameters:
+        
+        mesh: mesh object - the surface mesh
+        vl: Nvertices x Nvertices array - the normalized eddy-current modes vl[:,i]
+        fp: Nfieldpoints x 3 array - coordinates of the fieldpoints
+        freqs: Nfrequencies x 1 array - frequencies of interest
+        sigma: single - conductivity of the surface
+        d: single - thickness of the surface
+        T: single - temperature of the surface
+        
+    Returns:
+        
+        B: Nfrequencies x Nfieldpoints x 3components array - magnetic RMS noise
+    
+    '''
+    
+    kB = 1.38064852e-23 #Boltzmann constant
+    
+    #Compute field
     C = compute_C(mesh, fp)
     
+    #Compute mutual inductance at "hat basis"
     Mind = self_inductance_matrix(mesh.vertices,mesh.faces,mesh.face_normals)
-    Mind = 0.5*(Mind+Mind.T)
+    Mind = 0.5*(Mind+Mind.T) #I think that this enforces M to be symmetric
     
+    #Transform mutual inductance to eddy-current basis
     Mind_lap = vl.T@Mind@vl
     
+    #Eigendecomposition of M
     um_t, vm_t = eigh(Mind_lap)
     um_t = np.flip(um_t)
     vm_t = np.flip(vm_t,axis=1)
     
-    
+    #Let's take just the "99% variance" components to avoid numerical issues
     um = np.zeros(um_t.shape)
     vm = np.zeros(vm_t.shape)
     
@@ -79,6 +125,7 @@ def compute_ac_Bnoise(mesh, vl, fp, freqs, sigma, d, T):
     um[0:Ncomps] = um_t[0:Ncomps]
     vm[0:Ncomps,0:Ncomps] = vm_t[0:Ncomps,0:Ncomps]
     
+    #Compute B as a function of frequency
     B = np.zeros((freqs.shape[0], fp.shape[0],fp.shape[1]))
     
     Rk = 1/(sigma*d)
@@ -106,11 +153,24 @@ def compute_ac_Bnoise(mesh, vl, fp, freqs, sigma, d, T):
             B[j,:,2] += (C[:,:,2]@vec)**2
         print("Frequency %f computed" % (f))
         
-    B = np.sqrt(B)
+    B = np.sqrt(B) #RMS
     
     return B
 
 def visualize_current_modes(mesh,vl, Nmodes, scale, contours=True):
+    '''
+    Visualizes current modes up to Nmodes.
+    TODO: make this more flexible.
+    
+    Parameters:
+        
+        mesh: mesh object - the surface mesh
+        vl: Nvertices x Nvertices array - the normalized eddy-current modes vl[:,i]
+        Nmodes: single - Number of modes to be plotted
+        scale: single - scaling factor
+        contours: boolean - Show contours?
+
+    '''
     verts= mesh.vertices
     tris = mesh.faces
 
@@ -137,17 +197,22 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import trimesh
     
+    #Three different examples:
+    #   unit_sphere: DC Bnoise of a spherical shell at origin and comparison to analytical formula
+    #   unit_disc: DC Bnoise of a unit disc at z-axis and comparison to analytical formula
+    #   AC: AC Bnoise of a unit disc at one position
+    
     example_name = 'unit_sphere'
 #    example_name = 'unit_disc'
 #    example_name = 'AC'
     
+    #Fix the simulation parameters
     d = 100e-6
     sigma = 3.7e7
     T = 300
     kB = 1.38064852e-23
     mu0 = 4*np.pi*1e-7
         
-    
     if example_name == 'unit_sphere':
         Np = 10
         R = np.linspace(0.1, 1, Np)
@@ -164,7 +229,6 @@ if __name__ == '__main__':
             
             Btemp = compute_dc_Bnoise(mesh,vl,fp,sigma,d,T)
             B[i] = Btemp
-            
             
         visualize_current_modes(mesh,vl, 40, 5)
             
@@ -227,7 +291,7 @@ if __name__ == '__main__':
         fp[0,2] = 0.1
         
         Nfreqs = 30
-        freqs = np.logspace(0, 3, Nfreqs)
+        freqs = np.logspace(0, 3, Nfreqs) #30 frequencies from 1 to 1000 Hz 
         
         Bf = compute_ac_Bnoise(mesh,vl,fp,freqs,sigma,d,T)
         
@@ -243,5 +307,3 @@ if __name__ == '__main__':
         plt.grid(which='both')
         plt.tight_layout()
         
-
-    
