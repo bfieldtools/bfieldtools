d08dc78738d091303a3408a9ee7d66be6f4a267b
Antti Mäkinen <antti.makinen@aalto.fi>
2019-05-31 16:17:38 +0300

    Added gradient function to laplacian_mesh.py, and a corresponding example

--- bfieldtools/laplacian_mesh.py
+++ bfieldtools/laplacian_mesh.py
@@ -65,3 +65,53 @@ def mass_matrix(verts, tris, tri_areas=None, da=None):
 
     return A
 
+def gradient_matrix(verts, tris, tri_normals=None, tri_areas=None, rotated=False):
+    """ Calculate a (rotated) gradient matrix for hat basis functions
+        (stream functions) in the triangular mesh described by
+
+        verts: Nv x 3 array of mesh vertices (coordinates)
+        tris: Nt x 3 array of mesh triangles (indices to verts array)
+
+        return:
+            Gx ,Gy, Gx (Ntris, Nverts) matrices for calculating the components
+            of gradient at triangles
+    """
+    R = verts[tris]  # Nt x 3 (corners) x 3 (xyz)
+    # Calculate edge vectors for each triangle
+    edges = np.roll(R, 1, -2) - np.roll(R, 2, -2)  # Nt x 3 (edges) x 3 (x,y,z)
+
+    # Triangle normals and areas, compute if not provided
+    if isinstance(tri_normals, type(None)) or isinstance(tri_areas, type(None)):
+        tri_normals, tri_areas = tri_normals_and_areas(verts, tris)
+
+    tri_data = edges/(2*tri_areas[:, None, None])
+    if not rotated:
+        # Rotate 90 degrees CW to get the original gradient
+        tri_data = np.cross(tri_normals[:,None,:], tri_data, axis=-1)
+
+    tri_data = tri_data.reshape(-1, 3).T
+    ii = np.array([[i]*3 for i in range(len(tris))]).ravel()  # [0,0,0,1,1,1,...]
+    jj = tris.ravel()  # [t[0,0], t[0,1], t[0,2], t[1,0], t[1,1], t[1,2], ...]
+    Gx = csr_matrix((tri_data[0], (ii, jj)),
+                    shape=(tris.shape[0], verts.shape[0]), dtype=float)
+    Gy = csr_matrix((tri_data[1], (ii, jj)),
+                    shape=(tris.shape[0], verts.shape[0]), dtype=float)
+    Gz = csr_matrix((tri_data[2], (ii, jj)),
+                    shape=(tris.shape[0], verts.shape[0]), dtype=float)
+    return Gx, Gy, Gz
+
+def gradient(vals, verts, tris, tri_normals=None, tri_areas=None, rotated=False):
+    """ Calculate a (rotated) gradient for function values in hat basis
+        (stream functions) in the triangular mesh described by
+
+        verts: Nv x 3 array of mesh vertices (coordinates)
+        tris: Nt x 3 array of mesh triangles (indices to verts array)
+
+        return:
+            gradient (3, Ntris)
+    """
+    Gx, Gy, Gz = gradient_matrix(verts, tris, tri_normals, tri_areas, rotated)
+    return np.array([Gx @ vals, Gy @ vals, Gz @ vals])
+
+
+
