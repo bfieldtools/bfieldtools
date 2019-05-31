784d66819046c06f0827c4c77156ac388d354170
Lehto Iiro <iiro.lehto@aalto.fi>
2019-05-31 11:41:02 +0300

    fixed __init__.py

--- bfieldtools/__init__.py
+++ bfieldtools/__init__.py
@@ -6,6 +6,7 @@ from . import mesh_class
 from . import sphtools
 from . import thermal_noise
 from . import utils
+from . import mutual_inductance_mesh
 
 __all__=['bfield_line', 'coil_optimize', 'laplacian_mesh', 
-'magnetic_field_mesh', 'mesh_class', 'mutual_inductance_mesh', 'sphtools', 'thermal_noise', 'utils', 'validation']
+'magnetic_field_mesh', 'mesh_class', 'mutual_inductance_mesh', 'sphtools', 'thermal_noise', 'utils']#, 'validation']
