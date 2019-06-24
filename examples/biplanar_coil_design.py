124e7e5ab50cc462c377a9a8d00a80f3ee08195b
Rasmus Zetter <rasmus.zetter@aalto.fi>
2019-06-24 18:12:23 +0300

    Expanded on biplanar coil design example

--- examples/biplanar_coil_design.py
+++ examples/biplanar_coil_design.py
@@ -16,11 +16,11 @@ import pkg_resources
 
 #Set unit, e.g. meter or millimeter.
 # This doesn't matter, the problem is scale-invariant
-scaling_factor = 1e2
+scaling_factor = 1
 
 
 #Load simple plane mesh that is centered on the origin
-planemesh = trimesh.load(file_obj=pkg_resources.resource_filename('bfieldtools', 'example_meshes/10x10_plane.obj'), process=False)
+planemesh = trimesh.load(file_obj=pkg_resources.resource_filename('bfieldtools', 'example_meshes/10x10_plane_hires.obj'), process=False)
 
 planemesh.apply_scale(scaling_factor)
 
@@ -38,7 +38,7 @@ coil_minus = trimesh.Trimesh(planemesh.vertices + center_offset - standoff,
 joined_planes = coil_plus.union(coil_minus)
 
 #Create mesh class object
-coil = MeshWrapper(verts=joined_planes.vertices, tris=joined_planes.faces)
+coil = MeshWrapper(verts=joined_planes.vertices, tris=joined_planes.faces, fix_normals=False)
 
 #%% Set up target and stray field points
 
@@ -47,7 +47,7 @@ coil = MeshWrapper(verts=joined_planes.vertices, tris=joined_planes.faces)
 center = np.array([0, 0, 0]) * scaling_factor
 
 sidelength = 2 * scaling_factor
-n = 6
+n = 12
 xx = np.linspace(-sidelength/2, sidelength/2, n)
 yy = np.linspace(-sidelength/2, sidelength/2, n)
 zz = np.linspace(-sidelength/2, sidelength/2, n)
@@ -74,7 +74,7 @@ stray_radius = 20 * scaling_factor
 #                                   nalpha = 30,
 #                                   orientation=np.array([1, 0, 0]))
 #
-stray_points_mesh = trimesh.creation.icosphere(subdivisions=2, radius=stray_radius)
+stray_points_mesh = trimesh.creation.icosphere(subdivisions=4, radius=stray_radius)
 stray_points = stray_points_mesh.vertices + center
 
 n_stray_points = len(stray_points)
@@ -96,9 +96,9 @@ target_field = np.ones(target_points.shape[0], )
 # The tolerance parameter will determine the spatial detail of the coil.
 # Smaller tolerance means better but more intricate patterns. Too small values
 # will not be solveable.
-tolerance = 0.3
+tolerance = 0.2
 
-I, sol = optimize_streamfunctions(coil, target_field,
+coil.I, coil.sol = optimize_streamfunctions(coil, target_field,
                          target_axis=0,
                          target_error={'on_axis':0.01, 'off_axis':0.01, 'stray':0.01},
                          laplacian_smooth=0,
@@ -111,14 +111,14 @@ f = mlab.figure(None, bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5),
            size=(480, 480))
 mlab.clf()
 
-surface = mlab.pipeline.triangular_mesh_source(*coil.mesh.vertices.T, coil.mesh.faces,scalars=I)
+surface = mlab.pipeline.triangular_mesh_source(*coil.mesh.vertices.T, coil.mesh.faces,scalars=coil.I)
 
-windings = mlab.pipeline.contour_surface(surface, contours=8)
+windings = mlab.pipeline.contour_surface(surface, contours=10)
 
 
-B_target = np.vstack((coil.C[:, :, 0].dot(I),
-                  coil.C[:, :, 1].dot(I),
-                  coil.C[:, :, 2].dot(I))).T
+B_target = np.vstack((coil.C[:, :, 0].dot(coil.I),
+                  coil.C[:, :, 1].dot(coil.I),
+                  coil.C[:, :, 2].dot(coil.I))).T
 
 
 mlab.quiver3d(*target_points.T, *B_target.T)
@@ -128,7 +128,7 @@ mlab.quiver3d(*target_points.T, *B_target.T)
 
 plt.figure()
 
-z1 = np.linspace(0, 30, 31) * scaling_factor
+z1 = np.linspace(-30, 30, 101) * scaling_factor
 
 x1 = y1 = np.zeros_like(z1)
 
@@ -136,11 +136,11 @@ line1_points = np.vstack((x1, y1, z1)).T
 
 line1_C = compute_C(coil.mesh, r=line1_points)
 
-B_line1 = np.vstack((line1_C[:, :, 0].dot(I), line1_C[:, :, 1].dot(I), line1_C[:, :, 2].dot(I))).T
+B_line1 = np.vstack((line1_C[:, :, 0].dot(coil.I), line1_C[:, :, 1].dot(coil.I), line1_C[:, :, 2].dot(coil.I))).T
 
 plt.semilogy(z1 / scaling_factor, np.linalg.norm(B_line1, axis=1)/np.mean(np.abs(target_field)), label='Z')
 
-y2 = np.linspace(0, 30, 31) * scaling_factor
+y2 = np.linspace(-30, 30, 101) * scaling_factor
 
 z2 = x2 = np.zeros_like(y2)
 
@@ -148,7 +148,7 @@ line2_points = np.vstack((x2, y2, z2)).T
 
 line2_C = compute_C(coil.mesh, r=line2_points)
 
-B_line2 = np.vstack((line2_C[:, :, 0].dot(I), line2_C[:, :, 1].dot(I), line2_C[:, :, 2].dot(I))).T
+B_line2 = np.vstack((line2_C[:, :, 0].dot(coil.I), line2_C[:, :, 1].dot(coil.I), line2_C[:, :, 2].dot(coil.I))).T
 
 plt.semilogy(y2 / scaling_factor, np.linalg.norm(B_line2, axis=1)/np.mean(np.abs(target_field)), label='Y')
 plt.ylabel('Field amplitude (target field units)')
@@ -160,3 +160,123 @@ plt.grid(True, which='major', axis='x')
 plt.legend()
 
 plt.show()
+
+
+#%% Extract stream function isosurfaces/contours as polygons
+
+scene = mlab.figure(None, bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5),
+               size=(480, 480))
+mlab.clf()
+
+surface = mlab.pipeline.triangular_mesh_source(*coil.mesh.vertices.T, coil.mesh.faces,scalars=coil.I)
+
+#Compute contour values
+n_contours = 10
+#
+#    contours = []
+#
+#    I_max = np.max(coil[stack].I)
+#    I_min  = np.min(coil[stack].I)
+#    for contour_idx in range(1, n_contours+1):
+#        contours.append(I_min + (2 * contour_idx - 1) * (I_max - I_min) / (2 * n_contours))
+#
+
+
+windings = mlab.pipeline.contour_surface(surface, contours=n_contours)
+scene.scene.isometric_view()
+
+
+#Squeeze out the data from the contour plot. Ugly, I know.
+c=windings.trait_get('contour')['contour'].get_output_dataset()
+
+#points on the contour loops
+points = c.points.to_array()
+
+#contour line scalar value, sets current direction
+scalars = c.point_data.scalars.to_array()
+
+#Ugly, crappy structure containing continuous triangle edges for which
+# the second edge in the triangle is the edge of a loop.
+# These are not ordered in any sensible manner. Blergh.
+lines = c.lines.to_array()
+larr = np.asarray(lines).reshape([-1, 3])[:,1:]
+
+
+#Start by finding the separate loops
+loops = trimesh.graph.connected_components(larr)
+
+n_loops = len(loops)
+
+loop_polygons = []
+
+#Now go through the edges in each loop, one by one
+for loop_idx, loop in enumerate(loops):
+
+    loop_polygons.append([])
+    node_idx = loop[0] #Start with the first node we know is present in the loop
+
+    #Build a table of used nodes so we don't go around infinitely
+    node_used = [False]*len(points)
+
+    #Start looping
+    while node_idx < len(points):
+
+        if node_used[node_idx]:
+
+#                print('Encountered used node, stopping')
+
+            #Close loop by adding initial node
+            loop_polygons[loop_idx].append(node_idx)
+            break
+        else:
+            node_used[node_idx] = True
+            loop_polygons[loop_idx].append(node_idx)
+
+            #For which edge is our current node the FIRST node?
+            edge_idx = np.where(larr[:, 0] == node_idx)[0][0]
+
+            #Now take the SECOND node from that edge, that is our new current node
+            node_idx = larr[edge_idx, 1]
+
+#%% Compute magnetic field from discrete current line segments
+Bseg_target = np.zeros(B_target.shape)
+
+Bseg_line1 = np.zeros(B_line1.shape)
+Bseg_line2 = np.zeros(B_line2.shape)
+
+from bfieldtools.bfield_line import bfield_line_segments
+
+for loop_idx in range(n_loops):
+    Bseg_target += bfield_line_segments(points[loop_polygons[loop_idx]],
+                         target_points)
+
+    Bseg_line1 += bfield_line_segments(points[loop_polygons[loop_idx]],
+                         np.array([x1, y1, z1]).T)
+
+    Bseg_line2 += bfield_line_segments(points[loop_polygons[loop_idx]],
+                     np.array([x2, y2, z2]).T)
+
+
+plt.figure()
+
+I = 0.01
+plt.hist(1e9 * np.linalg.norm(Bseg_target, axis=1)*0.01, 50)
+plt.xlabel('Field amplitude in target region when %.1f mA current is injected (nT)' % (I*1e3))
+
+
+plt.figure()
+
+normalize_value = np.linalg.norm(Bseg_line1, axis=1)[np.where(z1==0)[0][0]]
+plt.semilogy(z1 / scaling_factor, np.linalg.norm(Bseg_line1, axis=1)/normalize_value, label='Z')
+
+normalize_value = np.linalg.norm(Bseg_line2, axis=1)[np.where(y2==0)[0][0]]
+plt.semilogy(y2 / scaling_factor, np.linalg.norm(Bseg_line2, axis=1)/normalize_value, label='Y')
+
+plt.ylabel('Field amplitude (target field units)')
+plt.xlabel('Distance from origin')
+plt.grid(True, which='minor', axis='y')
+plt.grid(True, which='major', axis='y', color='k')
+plt.grid(True, which='major', axis='x')
+plt.title('Field from discrete line segments, N_contours: %d'%n_contours)
+
+plt.legend()
