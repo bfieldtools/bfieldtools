5a086dac700d24a500d18bdc2984b050e2191872
Lius Elias <liuse1@nbe-078.org.aalto.fi>
2019-12-19 09:59:28 +0200

    Added function segment_looper, which can be used to sort the segments in order corresponding to the current direction.

--- bfieldtools/kicad_io.py
+++ bfieldtools/kicad_io.py
@@ -207,4 +207,158 @@ def shifted_kicad_to_python(file_directory, layers, orig_plane, scale_factor = 1
 
 
 
+def segment_looper(seg_starts, seg_ends, start_ind, up_low):
+    """
+    Author: Elias Lius
+    Arranges the current segments according to the current direction.
+    Inputs:
+        seg_starts: (N, 3) array
+            Start points of the segments
+        seg_ends: (N, 3) array
+            End points of the segments
+        start_ind: int
+            Index of the starting segment of the coil
+        up_low: int
+            Determines wether the segments are from upper or lower coil
+            (1 == upper, 0 == lower)
+    Outputs:
+        match_order: list
+            List of indices defining the correct order of the segments
+        no_match: list
+            List of segments with no matches with current specifications
+    """
+    
+    
+    match_order = [] #initialize match_order-list
+    no_match = []
+    indices = np.arange(len(seg_starts)) #initialize the list of indices of the segments
+    match_order.append(start_ind) #add the starting index to mathc_order
+    indices = np.delete(indices, start_ind) #delete the used index from indices to prevent using the index again
+    
+    matcher = seg_ends[start_ind] #The point to which we will next find a matching point
+    matcher_layer = 'front' #indicates is the point on outer or inner layer of the stack (e.g. F.Cu or In1.Cu)
+    
+    l_spacings = np.sort(np.abs(np.unique(seg_starts[:,2]))) #Coordinate of the two layers towards the plane norm
+    
+    #Next we loop over all the remaining segments and find their correct order
+    #We try to find the correct order by matching starting or ending points of the segment to previous segments ending point (ending point meaning the correct ending point according to current direction).
+    #Note that some segments may be drawn in wrong direction in kiCAD and hence we need to try to find the match from segments' ending and starting points.
+    #As we also have two layers for each stack, we need to take that into consideration aswell. 
+    while len(indices):
+        #First we divide the remaining segments to inner and outer layers, i.e., which segments are on front layer and which on back
+        match_list = seg_starts[indices]
+        if up_low == 1:
+            front_inds = indices[np.where(match_list[:, 2] == l_spacings[0])[0]] #indices for segments on front layer
+            back_inds = indices[np.where(match_list[:, 2] == l_spacings[1])[0]] #indices for segments on back layer
+        if up_low == 0:
+            front_inds = indices[np.where(match_list[:, 2] == -l_spacings[0])[0]] 
+            back_inds = indices[np.where(match_list[:, 2] == -l_spacings[1])[0]] 
+    
+        
+        #Next we will wind the closest matches from front and back layers and see which one is closer, and add that segment's index to match_order next.
+        #We use try-except condition to avoid errors in case of empty front_inds or back_inds.
+        try:
+            min_s_f = np.min(np.linalg.norm((seg_starts[front_inds,0:2] - matcher[0:2]),axis=1)) #Min to matcher distance from starting points on front layer
+            min_e_f = np.min(np.linalg.norm((seg_ends[front_inds,0:2] - matcher[0:2]),axis=1)) #Min to matcher disntance from ending points on front layer
+            
+            min_front = np.min(np.array([min_s_f, min_e_f])) #See which one has smaller distance to current matcher-point
+    
+            
+            #here we select the correct index of the point with minimum distance to the mathcer as match_ind_f (match_index_front)
+            if min_s_f == min_front:
+                match_ind_f = front_inds[(np.linalg.norm((seg_starts[front_inds,0:2] - matcher[0:2]),axis=1)).argmin()]
+                matcher_front = seg_ends[match_ind_f]
+                
+            if min_e_f == min_front:
+                match_ind_f = front_inds[(np.linalg.norm((seg_ends[front_inds,0:2] - matcher[0:2]),axis=1)).argmin()]
+                matcher_front = seg_starts[match_ind_f]
+    
+        #if error occurs we select min distance on front layer to be infinity
+        except:
+            min_front = np.inf
+            
+        
+        #Here we do the same thing for the back layer
+        try:
+            min_s_b = np.min(np.linalg.norm(seg_starts[back_inds,0:2] - matcher[0:2],axis=1))
+            min_e_b = np.min(np.linalg.norm(seg_ends[back_inds,0:2] - matcher[0:2],axis=1))
+            
+            min_back = np.min(np.array([min_s_b, min_e_b]))
+    
+            #Selection of the correct index of the point with smallest distance to matcher on back-layer as match_ind_b (match_index_back)
+            if min_s_b == min_back:
+                match_ind_b = back_inds[(np.linalg.norm(seg_starts[back_inds,0:2] - matcher[0:2],axis=1)).argmin()]
+                matcher_back = seg_ends[match_ind_b]
+                
+            if min_e_b == min_back:
+                match_ind_b = back_inds[(np.linalg.norm(seg_ends[back_inds,0:2] - matcher[0:2],axis=1)).argmin()]
+                matcher_back = seg_starts[match_ind_b]
+            
+        #if error occurs we select min distance on back layer to be infinity     
+        except:
+            min_back = np.inf
+    
+    
+    
+    
+        #in case of equal distances of the closest points on front and back layer, we select the point which is on the same layer as the matcher
+        if min_front<0.000127 and min_back<0.000127:
+            if matcher_layer == 'front':
+                matcher = matcher_front
+                match_order.append(match_ind_f)
+                indices = np.delete(indices, np.where(indices == match_ind_f))
+                matcher_layer = 'front'
+            if matcher_layer == 'back':
+                matcher = matcher_back
+                match_order.append(match_ind_b)
+                indices = np.delete(indices, np.where(indices == match_ind_b))
+                matcher_layer = 'back'
+                
+        #If both distances are over the limit, it might be due to layer switching by via. Thus, we check if the closest point to matcher lies on the other layer than matcher itself, and if so, accept
+        #it as a match. Otherwise, we move both closest points (front and back) to the no_match, list.
+        elif min_front > 0.000127 and min_back > 0.000127:
+            if min_front < min_back and matcher_layer == 'back':
+                matcher = matcher_front
+                match_order.append(match_ind_f)
+                indices = np.delete(indices, np.where(indices == match_ind_f))
+                matcher_layer = 'front'
+            elif min_back < min_front and matcher_layer == 'front':
+                matcher = matcher_back
+                match_order.append(match_ind_b)
+                indices = np.delete(indices, np.where(indices == match_ind_b))
+                matcher_layer = 'back'                
+            else:
+                no_match.append(match_ind_b)
+                indices = np.delete(indices, np.where(indices == match_ind_b))
+                no_match.append(match_ind_f)
+                indices = np.delete(indices, np.where(indices == match_ind_f))
+            
+        
+        #Now we can compare, which layer had the closest match to the matcher and select that point's index as the next value of match_order
+        elif min_front < min_back:
+            if min_front > 0.000127:
+                no_match.append(match_ind_f)
+                indices = np.delete(indices, np.where(indices == match_ind_f))
+            else:
+                matcher = matcher_front
+                match_order.append(match_ind_f)
+                indices = np.delete(indices, np.where(indices == match_ind_f))
+                matcher_layer = 'front'
+    
+            
+        elif min_front > min_back:
+            if min_back > 0.000127:
+                no_match.append(match_ind_b)
+                indices = np.delete(indices, np.where(indices == match_ind_b))
+            else:
+                matcher = matcher_back
+                match_order.append(match_ind_b)
+                indices = np.delete(indices, np.where(indices == match_ind_b))
+                matcher_layer = 'back'
+
+    return match_order, no_match
+
+
+
+
 
