5f7e430e7dde118328565944e7e65b319cd147ba
Rasmus Zetter <rasmus.zetter@aalto.fi>
2020-04-21 17:25:57 +0300

    Expand suhtools testing

--- tests/test_suhtools.py
+++ tests/test_suhtools.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mayavi import mlab
 
 from bfieldtools import suhtools
 from bfieldtools.utils import load_example_mesh
@@ -15,11 +16,11 @@ def test_suhbasis():
     for obj in [mesh, _fake_conductor(mesh_name="unit_disc")]:
         for mag in [False]:
             for bc in ["neumann", "dirichlet"]:
-                suh = suhtools.SuhBasis(
-                    mesh, Nc=10, boundary_condition=bc, magnetic=mag
-                )
+                suh = suhtools.SuhBasis(obj, Nc=10, boundary_condition=bc, magnetic=mag)
 
                 suh.field(suh.basis[1], np.array([[0, 0, 1]]))
+
+                suh.plot(Nfuncs=3)
 
     suh = suhtools.SuhBasis(mesh, Nc=10, boundary_condition="dirichlet", magnetic=True)
 
@@ -28,4 +29,13 @@ def test_suhbasis():
     except:
         print("Caught test exception")
 
-    suh.plot(Nfuncs=3)
+    f = mlab.figure()
+
+    suh.plot([0, 2], figure=f)
+    suh.plot(Nfuncs=4, Ncols=2, figure=f)
+
+
+@pytest.mark.xfail
+def test_suhbasis_fail():
+    mesh = load_example_mesh("unit_disc")
+    suh = suhtools.SuhBasis(mesh, Nc=10, boundary_condition="foo")
