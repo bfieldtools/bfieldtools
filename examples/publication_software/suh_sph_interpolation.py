a0337f758338e00403d969484d87915a03d73e2c
Antti Mäkinen <antti.makinen@aalto.fi>
2020-04-29 16:02:21 +0300

    Updated suh-sph interpolation comparison

--- examples/publication_software/suh_sph_interpolation.py
+++ examples/publication_software/suh_sph_interpolation.py
@@ -13,6 +13,7 @@ import mne
 SAVE_DIR = "./MNE interpolation/"
 
 #%%
+EVOKED = True
 
 with np.load(SAVE_DIR + "mne_data.npz", allow_pickle=True) as data:
     mesh = data["mesh"]
@@ -20,10 +21,34 @@ with np.load(SAVE_DIR + "mne_data.npz", allow_pickle=True) as data:
     n = data["n"]
     mesh = trimesh.Trimesh(vertices=data["vertices"], faces=data["faces"])
 
-evoked = mne.Evoked(SAVE_DIR + "left_auditory-ave.fif")
+if EVOKED:
+    evoked = mne.Evoked(SAVE_DIR + "left_auditory-ave.fif")
 
-i0, i1 = evoked.time_as_index(0.08)[0], evoked.time_as_index(0.09)[0]
-field = evoked.data[:, i0:i1].mean(axis=1)
+    i0, i1 = evoked.time_as_index(0.08)[0], evoked.time_as_index(0.09)[0]
+    field = evoked.data[:, i0:i1].mean(axis=1)
+
+else:
+    # take "data" from lead field matrix, i.e, topography of a single dipole
+    from mne.datasets import sample
+    import os
+
+    data_path = sample.data_path()
+
+    raw_fname = data_path + "/MEG/sample/sample_audvis_raw.fif"
+    trans = data_path + "/MEG/sample/sample_audvis_raw-trans.fif"
+    src = data_path + "/subjects/sample/bem/sample-oct-6-src.fif"
+    bem = data_path + "/subjects/sample/bem/sample-5120-5120-5120-bem-sol.fif"
+    subjects_dir = os.path.join(data_path, "subjects")
+
+    # Note that forward solutions can also be read with read_forward_solution
+    fwd = mne.make_forward_solution(
+        raw_fname, trans, src, bem, meg=True, eeg=False, mindist=5.0, n_jobs=2
+    )
+    # Take only magnetometers
+    mags = np.array([n[-1] == "1" for n in fwd["sol"]["row_names"]])
+    L = fwd["sol"]["data"][mags, :]
+    # Take the first dipole
+    field = L[:, 56]
 
 #%% radius for inner/outer sph
 
@@ -31,7 +56,7 @@ R = np.min(np.linalg.norm(p, axis=1)) - 0.02
 
 #%%
 
-lmax = 6  # maximum degree
+lmax = 9  # maximum degree
 Bca, Bcb = sphfield(p, lmax, normalization="energy", R=R)
 
 # sph-components at sensors
@@ -40,30 +65,45 @@ Bcb_sensors = np.einsum("ijk,ij->ik", Bcb, n)
 
 
 #%% Visualize sph components at the helmet
-idx = 20
+# idx = 20
 
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(Bca_sensors[:, idx].T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag", colorbar=False)
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(Bca_sensors[:, idx].T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag", colorbar=False)
 
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(Bcb_sensors[:, idx].T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag", colorbar=False)
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(Bcb_sensors[:, idx].T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag", colorbar=False)
 
 #%% calculate inner sph-coeffients with pinv
-alpha = np.linalg.pinv(Bca_sensors, rcond=1e-15) @ field
+PINV = False
+if PINV:
+    alpha = np.linalg.pinv(Bca_sensors, rcond=1e-15) @ field
+else:
+    ssa = np.linalg.svd(Bca_sensors @ Bca_sensors.T, False, False)
+    reg_exp = 6
+    _lambda = np.max(ssa) * (10 ** (-reg_exp))
+    # angular-Laplacian in the sph basis is diagonal
+    La = np.diag([l * (l + 1) for l in range(1, lmax + 1) for m in range(-l, l + 1)])
+    BB = Bca_sensors.T @ Bca_sensors + _lambda * La
+    alpha = np.linalg.solve(BB, Bca_sensors.T @ field)
 
+# Reconstruct field in helmet
 
-#%% reconstruct field in helmet
+# reco_sph = np.zeros(field.shape)
+# i = 0
+# for l in range(1, lmax + 1):
+#     for m in range(-1 * l, l + 1):
+#         reco_sph += alpha[i] * Bca_sensors[:, i]
+#         i += 1
 
-reco_sph = np.zeros(field.shape)
+# Produces the same result as the loop
+reco_sph = Bca_sensors @ alpha
 
-i = 0
-for l in range(1, lmax + 1):
-    for m in range(-1 * l, l + 1):
-        reco_sph += alpha[i] * Bca_sensors[:, i]
-        i += 1
-
+print(
+    "SPH-reconstruction relative error:",
+    np.linalg.norm(reco_sph - field) / np.linalg.norm(field),
+)
 
 #%%
 ##%% Fit the surface current for the auditory evoked response using pinv
@@ -98,24 +138,29 @@ s = StreamFunction(a, c)
 
 reco_suh = B_sensors @ s
 
-mlab.figure()
-surf = s.plot(False)
+print(
+    "SUH-reconstruction relative error:",
+    np.linalg.norm(reco_suh - field) / np.linalg.norm(field),
+)
+
+f = mlab.figure()
+surf = s.plot(False, figure=f)
 surf.actor.mapper.interpolate_scalars_before_mapping = True
 surf.module_manager.scalar_lut_manager.number_of_colors = 16
 
 #%% Plot the evoked and the reconsctructions
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(field.T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag")
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(field.T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag")
 
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(reco_sph.T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag")
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(reco_sph.T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag")
 
 
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(reco_suh.T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag")
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(reco_suh.T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag")
 
 
 #%% Plot spectra
@@ -133,11 +178,53 @@ U_sph = potential(
 
 U_suh = c.U_coupling(p) @ a
 
-#%%
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(U_sph.T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag")
+#%% MNE interpolates using SPHERICAL HEAD MODEL
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(U_sph.T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag")
 
-evoked1 = evoked.copy()
-evoked1.data[:, :] = np.tile(U_suh.T, (evoked.times.shape[0], 1)).T
-evoked1.plot_topomap(times=0.080, ch_type="mag")
+# evoked1 = evoked.copy()
+# evoked1.data[:, :] = np.tile(U_suh.T, (evoked.times.shape[0], 1)).T
+# evoked1.plot_topomap(times=0.080, ch_type="mag")
+
+
+#%% interpolate data on planar mesh
+from bfieldtools.utils import load_example_mesh
+from bfieldtools.mesh_calculus import gradient
+
+plane = load_example_mesh("10x10_plane_hires")
+scaling_factor = 0.03
+plane.apply_scale(scaling_factor)
+# Rotate to x-plane
+t = np.eye(4)
+theta = np.pi / 2 * 1.2
+t[1:3, 1:3] = np.array(
+    [[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]]
+)
+plane.apply_transform(t)
+
+c.U_coupling.reset()
+U_suh = c.U_coupling(plane.vertices) @ a
+# Adapt mesh to the function and calculate new points
+for i in range(2):
+    g = np.linalg.norm(gradient(U_suh, plane), axis=0)
+    face_ind = np.flatnonzero(g > g.max() * 0.05)
+    plane = plane.subdivide(face_ind)
+    U_suh = c.U_coupling(plane.vertices) @ a
+
+U_sph = potential(
+    plane.vertices, alpha, np.zeros(alpha.shape), lmax=lmax, normalization="energy", R=R
+)
+
+#%%
+from bfieldtools.viz import plot_data_on_vertices
+from bfieldtools.viz import plot_mesh
+
+# Mask inside/outside using solid angle
+mask = abs(c.U_coupling.matrix.sum(axis=1)) < 1e-6
+f = plot_data_on_vertices(plane, U_suh * mask, ncolors=15)
+# plot_mesh(mesh, figure=f)
+f = plot_data_on_vertices(plane, U_sph * mask, ncolors=15)
+# plot_mesh(mesh, figure=f)
+f = plot_data_on_vertices(plane, (U_suh - U_sph) * mask, ncolors=15)
+plot_mesh(mesh, figure=f)
