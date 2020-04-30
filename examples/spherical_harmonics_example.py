e757e249aa13b1578cf3e745d66a7e10903d2f22
Joonas Iivanainen <joonas.iivanainen@aalto.fi>
2020-04-30 17:17:38 +0300

    Updated sph example [skip ci]

--- examples/spherical_harmonics_example.py
+++ examples/spherical_harmonics_example.py
@@ -10,93 +10,86 @@ import matplotlib.pyplot as plt
 from bfieldtools.sphtools import SphBasis
 from bfieldtools import sphtools
 
-# Create basis object for evaluation of inner products etc.
-sph = SphBasis(20)
+# Create basis object for plotting, evaluation of inner products etc.
+sph = SphBasis(40)
 
-#%% Plot sphs up to l=4
+###############################################################################
+# Plot Spherical harmonics Ylms on the sphere
+# Plot Ylms up to l = 4
 mlab.figure()
-obj = sphtools.plotYlms(sph, 4)
+obj = sphtools.plotYlms(sph, lmax=4)
 
-# l=3, m=3
+# Plot Ylm with l=3, m=3
 mlab.figure()
-obj = sphtools.plotYlm(sph, 3, 3)
+obj = sphtools.plotYlm(sph, l=3, m=3)
 
-#%% Plot vector spherical harmonics
+###############################################################################
+# Plot vector spherical harmonics Vlms / Wlms on the sphere
 
-# Needs fixing: Plot sphere here
-# TODO: mesh info in sph (faces are missing!)
-
-offset = np.array((0, 0, 2))
+# Plot Vlm with l=2, m=2
 mlab.figure()
-obj = sphtools.plotBVlm_volume(sph, 5, 0, 1, 10, offset)
+obj = sphtools.plotVlm(sph, l=2, m=2)
+obj.scene.z_plus_view()
 
+# Plot Wlm with the same order and degree, l=2, m=2
 mlab.figure()
-obj = sphtools.plotBWlm_volume(sph, 5, 0, 1, 10, offset)
+obj = sphtools.plotWlm(sph, l=2, m=2)
+obj.scene.z_plus_view()
 
-#%% Test inner products
+###############################################################################
+# Plot fields of vector spherical harmonics Vlms / Wlms on a volume
 
-Vlm1 = sphtools.Wlm(1, 0, sph.sqp[:, 1], sph.sqp[:, 2])
-Vlm2 = sphtools.Wlm(7, 0, sph.sqp[:, 1], sph.sqp[:, 2])
+# Plot BVlm with l=2, m=2
+offset = np.array((0, 0, 3))  # add offset due to divergence in origin
+mlab.figure()
+obj = sphtools.plotBVlm_volume(sph, l=2, m=2, lim=1, Np=10, offset=offset)
 
-print(sph.innerproduct(Vlm1, Vlm2))
+# Plot BWlm with l=2, m=2
+offset = np.array((0, 0, 0))  # include origin
+mlab.figure()
+obj = sphtools.plotBWlm_volume(sph, l=2, m=2, lim=1, Np=10, offset=offset)
+
+###############################################################################
+# Test inner products in quadrature points defined in sph-object
+
+Ylm1 = sphtools.ylm(2, 1, sph.sqp[:, 1], sph.sqp[:, 2])
+Ylm2 = sphtools.ylm(2, 0, sph.sqp[:, 1], sph.sqp[:, 2])
+print(
+    "Inner product between Ylm_20 and Ylm_21 is %f"
+    % sph.innerproduct(Ylm1[:, None], Ylm2[:, None])
+)
+print(
+    "Inner product between Ylm_20 and Ylm_20 is %f"
+    % sph.innerproduct(Ylm2[:, None], Ylm2[:, None])
+)
+
+Vlm1 = sphtools.Vlm(1, 0, sph.sqp[:, 1], sph.sqp[:, 2])
+Vlm2 = sphtools.Vlm(7, 0, sph.sqp[:, 1], sph.sqp[:, 2])
+print("Inner product between V_10 and V_70 is %f" % sph.innerproduct(Vlm1, Vlm2))
+print("Inner product between V_10 and V_10 is %f" % sph.innerproduct(Vlm1, Vlm1))
 
 Wlm1 = sphtools.Wlm(1, 0, sph.sqp[:, 1], sph.sqp[:, 2])
 Wlm2 = sphtools.Wlm(7, 0, sph.sqp[:, 1], sph.sqp[:, 2])
+print("Inner product between W_10 and W_70 is %f" % sph.innerproduct(Wlm1, Wlm2))
+print("Inner product between W_10 and W_10 is %f" % sph.innerproduct(Wlm1, Wlm1))
 
-print(sph.innerproduct(Wlm2, Wlm2))
+print("Inner product between W_10 and V_10 is %f" % sph.innerproduct(Wlm1, Vlm1))
 
-#%% Example of a spectrum
+###############################################################################
+# Simple example of the calculation of sph spectrum of magnetic field
+# defined on the sphere using the inner product.
+
+# define homogeneous magnetic field along z-axis
 B = np.zeros(sph.sqp.shape)
-# B[:,0] = 1
 B[:, 2] = sph.qp.points[:, 0] / np.max(sph.qp.points[:, 0])
+
+# add noise
 B += 0.1 * np.random.randn(B.shape[0], B.shape[1])
 
-B = sphtools.cartvec2sph(sph.sqp, B)
+B = sphtools.cartvec2sph(sph.sqp, B)  # transform to spherical coordinates
 
-coeffs = sph.avsphspectra(B, 7)  # OK??
+coeffs = sph.avsphspectra(B, 7)  # calculate spectra of the r**l terms
 
+# Plot the spectra
 plt.figure()
 plt.semilogy(coeffs ** 2)
-
-# obj = sphtools.plotYlm(sph, 5, 3)
-
-#%% Plot potential on a flat mesh
-Np = 10
-lim = 3
-x, y, z = np.meshgrid(
-    np.linspace(-lim, lim, Np), np.linspace(-lim, lim, Np), np.linspace(-lim, lim, Np)
-)
-
-
-#    p = np.array((x.flatten(), y.flatten(), z.flatten())).T
-
-p = np.array((x.flatten(), y.flatten(), np.zeros(y.flatten().shape))).T
-lmax = 2
-acoeffs = np.zeros(lmax * (lmax + 2))
-bcoeffs = np.zeros(lmax * (lmax + 2))
-acoeffs[7] = 1
-#    bcoeffs[2] = 1
-
-pot = sphtools.potential(p, acoeffs, bcoeffs, lmax)
-
-pot = np.reshape(pot, x.shape)
-
-mlab.figure()
-mlab.mesh(x[:, :, 0], y[:, :, 0], z[:, :, 0], scalars=pot[:, :, 0], colormap="Spectral")
-
-# #%% Fit spectrum to random data
-# coords = np.zeros((p.shape[0], p.shape[1], 3))
-# coords[:, :, 0] = p
-# coords[:, :, 1] = p
-# coords[:, :, 2] = p
-
-# B = np.zeros((coords.shape[0], coords.shape[1]))
-# B[:, 2] = p[:, 0] / np.max(p[:, 0])
-# B[:, 1] = 0.3
-# B += 0.4 * np.random.randn(B.shape[0], B.shape[1])
-
-# lmax = 5
-# coeffs, coeffs2, mse = sphtools.fit_spectra(coords, B, lmax)
-
-# plt.figure()
-# plt.semilogy(coeffs ** 2, ".")
