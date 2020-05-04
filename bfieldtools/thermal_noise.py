aed8fa7e4b197b8ff9d0cc26f7c475842a39c77e
Joonas Iivanainen <joonas.iivanainen@aalto.fi>
2020-05-04 16:51:22 +0300

    Single function to compute noise currents. Major update on the examples

--- bfieldtools/thermal_noise.py
+++ bfieldtools/thermal_noise.py
@@ -4,80 +4,113 @@ Contains functions for computing thermal noise in conductive thin objects.
 """
 
 import numpy as np
-from scipy.linalg import eigh
-from scipy.sparse.linalg import eigsh
-from scipy.linalg import eigh  # , eigsh
 from mayavi import mlab
+import trimesh
 
 from .suhtools import SuhBasis
-from .mesh_magnetics import magnetic_field_coupling
-from .mesh_calculus import laplacian_matrix, mass_matrix
-from .mesh_impedance import self_inductance_matrix, resistance_matrix
-from . import utils
+from .mesh_conductor import MeshConductor
 
 
-def compute_AC_current_modes(
+def compute_current_modes(
     obj,
-    freqs,
+    T,
     resistivity,
     thickness,
-    T,
-    closed=True,
+    mode="AC",
+    freqs=np.array((0,)),
     Nmodes=None,
     return_eigenvals=False,
+    **kwargs
 ):
     """
+    Calculates the (AC or DC) Johnson noise current modes on the conducting surface.
+    
     Parameters
     ----------
-    obj : Trimesh-object or MeshConductor-object
+    obj: Trimesh-object or MeshConductor-object
         Represents the boundary on which current density is specified
         or MeshConductor object that wraps the mesh
-    freqs: Nfreqs array
-        The frequencies at which the eddy-current modes are computed
     T: float
         Temperature in Kelvins
-    closed: boolean
-        Is the mesh closed (True) or not (False)
+    resistivity: float or array (Nfaces)
+        Resistivity value in Ohm/meter
+    thickness: float or array (Nfaces)
+        Thickness of the surface. NB! Must be small in comparison to observation distance
+    mode: 'AC' or 'DC'
+        Calculate modes as a function of frequency or just at DC?
+    freqs: Nfreqs array
+        The frequencies at which the eddy-current modes are computed. Obsolete for 
+        'DC' mode. In 'AC', calculate at DC in default.
     Nmodes: int
         How many modes are computed? If None, all Nvertices modes are computed
     return_eigenvals: boolean
         Return also the eigenvalues (the inverse circuit time constants)?
-
+    kwargs: dict
+        Passed to Conductor creation if a Trimesh object is passed as 'obj'
+        
     Returns
     -------
-    vl: (Nvertices x Nmodes x Nfreqs) array
+    vl: (Nvertices x Nmodes x Nfreqs) (AC) or (Nvertices x Nmodes) (DC) array
         The spectral eddy-current modes
     u: Nmodes array
-        The eigenvalues (the inverse circuit time constants)
-
+        The eigenvalues
     """
 
     kB = 1.38064852e-23
 
-    suh = SuhBasis(
-        obj, Nc=Nmodes, magnetic="AC", resistivity=resistivity, thickness=thickness
-    )
+    if isinstance(obj, MeshConductor):
+        obj.resistivity = resistivity
+        obj.thickness = thickness
+        print(
+            "Updated MeshConductor object thickness and resistivity attributes according to function parameters"
+        )
+    elif isinstance(obj, trimesh.Trimesh):
+        obj = MeshConductor(
+            mesh_obj=obj,
+            resistivity=resistivity,
+            thickness=thickness,
+            resistance_full_rank=False,
+            **kwargs
+        )
+    else:
+        raise TypeError("obj type should be either Trimesh or Conductor")
+
+    if mode == "AC":
+        suh = SuhBasis(obj, Nc=Nmodes, magnetic="AC")
+    elif mode == "DC":
+        suh = SuhBasis(obj, Nc=Nmodes, magnetic="DC")
+    else:
+        raise ValueError("Mode should be either 'AC' or 'DC'")
 
     v = suh.basis
     u = suh.eigenvals
 
     Nfreqs = len(freqs)
 
-    # Scale the eigenmodes with the spectral density of the thermal noise current
-    vl = np.zeros((len(suh.mesh_conductor.mesh.vertices), v.shape[1], Nfreqs))
+    if mode == "AC":
+        Nfreqs = len(freqs)
+        vl = np.zeros((len(suh.mesh_conductor.mesh.vertices), v.shape[1], Nfreqs))
 
-    for i in range(v.shape[1]):
-        amp = (
-            2
-            * np.sqrt(kB * T / u[i])
-            * np.sqrt(1 / (1 + (2 * np.pi * freqs / u[i]) ** 2))
-        )
-        vl[:, i, :] = (
-            (
-                np.zeros((Nfreqs, vl.shape[0]))
-                + suh.mesh_conductor.inner2vert @ v[:, i]
-            ).T
-        ) * amp
+        # Scale the eigenmodes with the spectral density of the thermal noise current
+        for i in range(v.shape[1]):
+            amp = (
+                2
+                * np.sqrt(kB * T / u[i])
+                * np.sqrt(1 / (1 + (2 * np.pi * freqs / u[i]) ** 2))
+            )
+            vl[:, i, :] = (
+                (
+                    np.zeros((Nfreqs, vl.shape[0]))
+                    + suh.mesh_conductor.inner2vert @ v[:, i]
+                ).T
+            ) * amp
+    elif mode == "DC":
+        vl = np.zeros((len(suh.mesh_conductor.mesh.vertices), v.shape[1]))
+
+        # Scale the eigenmodes with the spectral density of the thermal noise current
+        for i in range(v.shape[1]):
+            amp = 2 * np.sqrt(kB * T / u[i])
+            vl[:, i] = suh.mesh_conductor.inner2vert @ v[:, i] * amp
 
     if return_eigenvals:
         return vl, u
@@ -85,56 +118,25 @@ def compute_AC_current_modes(
         return vl
 
 
-def compute_DC_current_modes(
-    obj, resistivity, thickness, T, closed=True, Nmodes=None, return_eigenvals=False
-):
+def noise_covar(B_coupling, vl, Nmodes=None):
     """
+    Calculates (AC or DC) magnetic noise covariance along x, y and z from the 
+    modes vl.
+    
     Parameters
     ----------
-    obj : Trimesh-object or MeshConductor-object
-        Represents the boundary on which current density is specified
-        or MeshConductor object that wraps the mesh
-    T: float
-        Temperature in Kelvins
-    closed: boolean
-        Is the mesh closed (True) or not (False)
+    B_coupling: ndarray (Np, 3, Nvertices)
+        Magnetic field coupling matrix from the mesh
+    vl: ndarray (Nvertices, Nmodes, x Nfreqs) or (Nvertices, Nmodes)
+        The Johnson noise current modes on the mesh
     Nmodes: int
-        How many modes are computed? If None, all Nvertices modes are computed
-    return_eigenvals: boolean
-        Return also the eigenvalues (the inverse circuit time constants)?
+        How many modes are included? If None, all modes in vl are included
 
     Returns
     -------
-    vl: (Nvertices x Nmodes x Nfreqs) array
-        The spectral eddy-current modes
-    u: Nmodes array
-        The eigenvalues (the inverse circuit time constants)
-
+    Bcov: ndarray (Np, Np, 3, Nfreqs) or (Np, Np, 3)
+        Magnetic noise covariance
     """
-
-    kB = 1.38064852e-23
-
-    suh = SuhBasis(
-        obj, Nc=Nmodes, magnetic="DC", resistivity=resistivity, thickness=thickness
-    )
-
-    v = suh.basis
-    u = suh.eigenvals
-
-    # Scale the eigenmodes with the spectral density of the thermal noise current
-    vl = np.zeros((len(suh.mesh_conductor.mesh.vertices), v.shape[1]))
-
-    for i in range(v.shape[1]):
-        amp = 2 * np.sqrt(kB * T / u[i])
-        vl[:, i] = suh.mesh_conductor.inner2vert @ v[:, i] * amp
-
-    if return_eigenvals:
-        return vl, u
-    else:
-        return vl
-
-
-def noise_covar(mesh, B_coupling, vl, Nmodes=None):
     if Nmodes == None:
         Nmodes = vl.shape[1]
 
@@ -148,7 +150,25 @@ def noise_covar(mesh, B_coupling, vl, Nmodes=None):
     return Bcov
 
 
-def noise_var(mesh, B_coupling, vl, Nmodes=None):
+def noise_var(B_coupling, vl, Nmodes=None):
+    """
+    Calculates (AC or DC) magnetic noise variance along x, y and z from the 
+    modes vl.
+    
+    Parameters
+    ----------
+    B_coupling: ndarray (Np, 3, Nvertices)
+        Magnetic field coupling matrix from the mesh
+    vl: ndarray (Nvertices, Nmodes, x Nfreqs) or (Nvertices, Nmodes)
+        The Johnson noise current modes on the mesh
+    Nmodes: int
+        How many modes are included? If None, all modes in vl are included
+
+    Returns
+    -------
+    Bcov: ndarray (Np, 3, Nfreqs) or (Np, 3)
+        Magnetic noise variance
+    """
     if Nmodes == None:
         Nmodes = vl.shape[1]
 
@@ -162,7 +182,25 @@ def noise_var(mesh, B_coupling, vl, Nmodes=None):
     return Bcov
 
 
-def noise_covar_dir(mesh, B_coupling, vl, Nmodes=None):
+def noise_covar_dir(B_coupling, vl, Nmodes=None):
+    """
+    Calculates (AC or DC) magnetic noise covariance between x, y and z directions
+    from the modes vl.
+    
+    Parameters
+    ----------
+    B_coupling: ndarray (Np, 3, Nvertices)
+        Magnetic field coupling matrix from the mesh
+    vl: ndarray (Nvertices, Nmodes, x Nfreqs) or (Nvertices, Nmodes)
+        The Johnson noise current modes on the mesh
+    Nmodes: int
+        How many modes are included? If None, all modes in vl are included
+
+    Returns
+    -------
+    Bcov: ndarray (Np, 3, 3, Nfreqs) or (Np, 3, 3)
+        Magnetic noise covariance x, y and z field components
+    """
     if Nmodes == None:
         Nmodes = vl.shape[1]
 
