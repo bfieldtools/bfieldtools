8e03ff3939e6997f7b0730998f34c655eb498128
Joonas Iivanainen <joonas.iivanainen@aalto.fi>
2020-05-12 11:47:21 +0300

    Added a new test using disc and analytic solution

--- tests/test_thermal_noise.py
+++ tests/test_thermal_noise.py
@@ -2,7 +2,9 @@ import numpy as np
 
 from bfieldtools import thermal_noise
 from bfieldtools.utils import load_example_mesh
+from bfieldtools.mesh_magnetics import magnetic_field_coupling
 
+import trimesh
 import pytest
 
 from numpy.testing import (
@@ -47,3 +49,56 @@ def test_compute_modes():
     assert_allclose(DC_Bn_var, AC_Bn_var[:, :, 0])
 
     thermal_noise.visualize_current_modes(obj.mesh, DC_n, scale=0.5, Nmodes=3)
+
+
+def test_vs_analytic():
+
+    obj = _fake_mesh_conductor("unit_disc")
+    mesh = obj.mesh
+
+    mesh.vertices, mesh.faces = trimesh.remesh.subdivide(mesh.vertices, mesh.faces)
+
+    # Fix the simulation parameters
+    d = 100e-6  # thickness
+    sigma = 3.7e7  # conductivity
+    res = 1 / sigma  # resistivity
+    T = 300  # temperature
+    kB = 1.38064852e-23  # Boltz
+    mu0 = 4 * np.pi * 1e-7  # permeability of freespace
+
+    # Compute the AC-current modes and visualize them
+    vl, u = thermal_noise.compute_current_modes(
+        obj=mesh, T=T, resistivity=res, thickness=d, mode="AC", return_eigenvals=True
+    )
+
+    # Define field points on z axis
+    Np = 10
+    z = np.linspace(0.2, 1, Np)
+    fp = np.array((np.zeros(z.shape), np.zeros(z.shape), z)).T
+
+    B_coupling = magnetic_field_coupling(
+        mesh, fp, analytic=True
+    )  # field coupling matrix
+
+    # Compute noise variance
+    B = np.sqrt(thermal_noise.noise_var(B_coupling, vl))
+
+    # Next, we compute the DC noise without reference to the inductance
+    vl_dc, u_dc = thermal_noise.compute_current_modes(
+        obj=mesh, T=T, resistivity=res, thickness=d, mode="DC", return_eigenvals=True
+    )
+
+    # Compute noise variance
+    B_dc = np.sqrt(thermal_noise.noise_var(B_coupling, vl_dc))
+
+    # Calculate Bz noise using analytical formula and plot the results
+    r = 1
+    Ban = (
+        mu0
+        * np.sqrt(sigma * d * kB * T / (8 * np.pi * z ** 2))
+        * (1 / (1 + z ** 2 / r ** 2))
+    )
+
+    assert_allclose(B_dc[:, 2], B[:, 2, 0])
+
+    assert_allclose(Ban, B[:, 2, 0], rtol=5e-2)
